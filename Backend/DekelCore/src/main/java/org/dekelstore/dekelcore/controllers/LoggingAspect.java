32d084b540e233225af5c3bd6a77bba5f519cbb2
JuDaOcVa <juandavidocampo80@gmail.com>
2024-02-26 07:37:55 -0500

    Update logging methods extracting parameters to print if exist

--- Backend/DekelCore/src/main/java/org/dekelstore/dekelcore/controllers/LoggingAspect.java
+++ Backend/DekelCore/src/main/java/org/dekelstore/dekelcore/controllers/LoggingAspect.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Aspect
 @Component
 public class LoggingAspect {
@@ -17,11 +20,15 @@ public class LoggingAspect {
     @Before("execution(*  org.dekelstore.dekelcore..*.*(..))")
     public void logBeforeMethodExecution(JoinPoint joinPoint) {
         logger.info("{{LOG}} Before method execute: " + joinPoint.getSignature().toShortString());
+        if (Arrays.stream(joinPoint.getArgs()).anyMatch(Objects::nonNull))
+            logger.info("{{LOG}} Before method execute: " + Arrays.toString(joinPoint.getArgs()));
     }
 
     @AfterReturning(pointcut = "execution(*  org.dekelstore.dekelcore..*.*(..))", returning = "result")
     public void logAfterMethodExecution(JoinPoint joinPoint, Object result) {
         logger.info("{{LOG}} After method execute: " + joinPoint.getSignature().toShortString());
+        if (Arrays.stream(joinPoint.getArgs()).anyMatch(Objects::nonNull))
+            logger.info("{{LOG}} After method execute: " + Arrays.toString(joinPoint.getArgs()));
         logger.info("{{LOG}} Result : " + result);
     }
 }
